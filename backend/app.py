6f76f617a0cb011fda399f3a9181f317486d4802
Ali Asgar Padaria <aliasgarpadaria002@gmail.com>
2025-02-09 09:12:51 -0500

    Feature: Added RAG to get personal user data

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
 load_dotenv()
 import google.generativeai as genai
-
+from classify_questions import classify_question
 
 
 app = Flask(__name__)
@@ -95,21 +95,22 @@ def query():
         }
         messages = [system_message] + [{"role": message['role'], "content": message['content']} for message in conversation]
 
+        #fetch the last question from the conversation
+        question = messages[-1]['content']
 
-        print("Fetvched Questions successfully")
-        classification = "financial education"
-        
-        if classification == "stock market":
-            # response = invest(user_id, question)
-            print("In Stock Market")
-        elif classification == "personal budgeting":
+        classification = classify_question(question).strip().lower()
+        print("Classification : ", classification)
+
+        if classification == "personal budgeting":
             # response = personal_budgeting(user_id, question)
             print("In Personal Budgeting")
+            response = personal_budgeting(user_id, question)
         elif classification == "financial education":
             print("In Financial Education")
             response = financial_education(user_id, messages)
         else:
-            return jsonify({'error': 'Invalid classification'})
+            print("In other")
+            response = financial_education(user_id, messages)
 
         if response['status_code'] == 200:
             return jsonify({'message': 'Successful', 'response': response['message']}), 200
@@ -138,7 +139,39 @@ def financial_education(user_id, question):
     except Exception as e:
         return {'error': f"An error occurred: {str(e)}", 'status_code': 500}
 
+def personal_budgeting(user_id, question):
+    # in this, after the prompt, add the data from pickle files and add a line saying sue the above data to answer the question if it is relevant and stuff.
 
+    try:
+        user_folder = os.path.join(folder_name, user_id)
+        vector_store_path = user_folder
+        vs = FAISS.load_local(str(vector_store_path),
+                          embeddings=embed, allow_dangerous_deserialization=True)
+        all_docs = vs.docstore._dict.values()
+        if all_docs:
+            all_docs_data = "\n".join([doc.page_content for doc in all_docs])
+            print( "\n".join([doc.page_content for doc in all_docs]))
+        else:
+            print("No documents available in the database.")
+
+        prompt = f"{question}"
+        # add add_docs_data to the prompt
+        prompt = prompt + "\n" + all_docs_data
+        prompt = prompt + "\n" + "given the question, answer it based on the database provided and analyze that database and give answer. You can also add on information if you feel something is missing in the database, if there is nothing in the database related generate an appropriate generate response and do not explicitly state that there is nothing in the database in response."
+        print("Setting up a model")
+        print("Question : ", prompt)
+        model = genai.GenerativeModel("gemini-1.5-flash-002")
+        print('Model:', model)
+        response = model.generate_content(prompt)
+
+        if response and response.text:
+            generated_text = response.text
+            return {'question': question, 'message': generated_text, 'status_code': 200}
+        else:
+            return {'error': 'No response from the model', 'status_code': 500}
+
+    except Exception as e:
+        return {'error': f"An error occurred: {str(e)}", 'status_code': 500}
 
 if __name__ == '__main__':
     app.run(debug=True)
